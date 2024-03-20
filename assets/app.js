bf3b3c34543777c305ca555557e97d79a6c8dfca
Ceeessan <Cessan__94@hotmail.com>
2024-03-20 18:46:24 +0100

    ändringar i product-page.

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var currentPage = window.location.pathname;
+  if (currentPage === "/" || currentPage === "/home") {
+    document.querySelector(".announcement-bar").style.display = "block";
+  } else {
+    document.querySelector(".announcement-bar").innerText = "Alla barnkläder -25%";
+    document.querySelector(".announcement-bar").style.color = "white";
+    document.querySelector(".announcement-bar").style.fontSize = "14px";
+  }
+});
 const productContainer = document.querySelector("slider-component");
 const clonedProductContainer = productContainer.cloneNode(true);
 const scrollContainer = document.querySelector(".products-again");
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,20 @@
 
-//Skapa en kopia av produkterna så att dem kan visas längre ner av front-page.
+//Ändra texten för announcement-bar.
+document.addEventListener("DOMContentLoaded", function () {
+    var currentPage = window.location.pathname;
+
+    if (currentPage === '/' || currentPage === '/home') {
+        document.querySelector('.announcement-bar').style.display = 'block';
+    } else {
+        document.querySelector('.announcement-bar').innerText = 'Alla barnkläder -25%';
+        document.querySelector('.announcement-bar').style.color = 'white';
+        document.querySelector('.announcement-bar').style.fontSize = '14px';
+    }
+});
+
+
+
+//Skapa en kopia av produkterna så att dem kan visas längre ner på front-page.
 const productContainer = document.querySelector('slider-component');
 
 
